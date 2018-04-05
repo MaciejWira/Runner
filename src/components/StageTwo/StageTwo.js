f523fdc1549731bfd49d911e83400ff134109841
Maciej Wira <wira.maciej@gmail.com>
2018-04-05 10:01:08 +0200

    StageTwo jako osobny komponent

--- src/components/StageTwo/StageTwo.js
+++ src/components/StageTwo/StageTwo.js
@@ -5,20 +5,32 @@ import Tempo from '../Tempo/Tempo';
 
 const stageTwo = (props) => {
 
-  let stageTwoParameters = [];
-  if (props.distance) stageTwoParameters.push(<Distance
-                                              key="distance"
-                                              km={this.state.distance.km}
-                                              kmUp={this.changeValue.bind(this, "plus", "distance", "km")}
-                                              kmDown={this.changeValue.bind(this, "minus", "distance", "km")}
-                                              m={this.state.distance.m}
-                                              mUp={this.changeValue.bind(this, "plus", "distance", "m")}
-                                              mDown={this.changeValue.bind(this, "minus", "distance", "m")}/>);
-  if (props.tempo) stageTwoParameters.push(<Tempo key="tempo"/>);
-  if (props.time) stageTwoParameters.push(<Time key="time"/>);
+  let state = props.state;
 
-  return(
-    {stageTwoParameters}
+  let stageTwoParameters = [];
+  if (state.starters.parameters.distance) stageTwoParameters.push(<Distance
+                                                              key="distance"
+                                                              km={state.distance.km}
+                                                              kmUp={props.changeValue.bind(this, "plus", "distance", "km")}
+                                                              kmDown={props.changeValue.bind(this, "minus", "distance", "km")}
+                                                              m={state.distance.m}
+                                                              mUp={props.changeValue.bind(this, "plus", "distance", "m")}
+                                                              mDown={props.changeValue.bind(this, "minus", "distance", "m")}/>);
+  if (state.starters.parameters.tempo) stageTwoParameters.push(<Tempo
+                                                          key="tempo"
+                                                          min={state.tempo.min}
+                                                          minUp={props.changeValue.bind(this, "plus", "tempo", "min")}
+                                                          minDown={props.changeValue.bind(this, "minus", "tempo", "min")}
+                                                          s={state.tempo.s}
+                                                          sUp={props.changeValue.bind(this, "plus", "tempo", "s")}
+                                                          sDown={props.changeValue.bind(this, "minus", "tempo", "s")}/>);
+  if (state.starters.parameters.time) stageTwoParameters.push(<Time key="time"/>);
+
+  return (
+    <div>
+      {stageTwoParameters}
+      <button onClick={props.changeStage}>DALEJ</button>
+    </div>
   );
 
 };
--- src/containers/Stage/Stage.js
+++ src/containers/Stage/Stage.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import StageOne from '../../components/StageOne/StageOne';
-import Distance from '../../components/Distance/Distance';
-import Time from '../../components/Time/Time';
-import Tempo from '../../components/Tempo/Tempo';
+import StageTwo from '../../components/StageTwo/StageTwo';
 
 class Stage extends Component {
 
@@ -78,7 +76,7 @@ class Stage extends Component {
     };
   }
 
-  changeValue = (direction, parameter, subparameter) => {
+  changeValueHandler = (direction, parameter, subparameter) => {
     let newParameter = {...this.state[parameter]};
     if (direction === "plus") newParameter[subparameter]++;
     else if (direction === "minus" && newParameter[subparameter] > 0) newParameter[subparameter]--;
@@ -89,36 +87,15 @@ class Stage extends Component {
 
   render(){
 
-    let stageTwo = [];
-    if (this.state.starters.parameters.distance) stageTwo.push(<Distance
-                                                                key="distance"
-                                                                km={this.state.distance.km}
-                                                                kmUp={this.changeValue.bind(this, "plus", "distance", "km")}
-                                                                kmDown={this.changeValue.bind(this, "minus", "distance", "km")}
-                                                                m={this.state.distance.m}
-                                                                mUp={this.changeValue.bind(this, "plus", "distance", "m")}
-                                                                mDown={this.changeValue.bind(this, "minus", "distance", "m")}/>);
-    if (this.state.starters.parameters.tempo) stageTwo.push(<Tempo
-                                                            key="tempo"
-                                                            min={this.state.tempo.min}
-                                                            minUp={this.changeValue.bind(this, "plus", "tempo", "min")}
-                                                            minDown={this.changeValue.bind(this, "minus", "tempo", "min")}
-                                                            s={this.state.tempo.s}
-                                                            sUp={this.changeValue.bind(this, "plus", "tempo", "s")}
-                                                            sDown={this.changeValue.bind(this, "minus", "tempo", "s")}/>);
-    if (this.state.starters.parameters.time) stageTwo.push(<Time key="time"/>);
-
     const presentStage = () => {
       if (this.state.stage[0]) return <StageOne
                                       changeStage={this.changeStageHandler}
                                       chooseStarters={this.startersHandler}
                                       checkStarters={this.checkStartersHandler}/>;
-      if (this.state.stage[1]) return (
-        <div>
-          {stageTwo}
-          <button onClick={this.changeStageHandler}>DALEJ</button>
-        </div>
-      );
+      if (this.state.stage[1]) return <StageTwo
+                                      state={this.state}
+                                      changeValue={this.changeValueHandler}
+                                      changeStage={this.changeStageHandler}/>;
       if (this.state.stage[2]) return (
         <div>
           "General Kenobi..."
